319b99f9e5e970f0b2ecfe2417c65cadabee9a0a
danWagar <dandwagar@gmail.com>
2019-09-19 09:29:01 -0700

    cats

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function handleThumnailClicks() {
-    $('.thumbnail').on('click', function() {
-        $('.event').children('img').remove()
-    };
-};
-$('.thumbnail').on('click', e => {
-    console.log(e.target);
+  $('.thumbnail').on('click', function(e) {
+    let hero = $('.hero');
+    console.dir(hero.children('img'));
+    hero.children('img').remove();
+    image = $(this).find('img');
+    let src = image.attr('src');
+    let alt = image.attr('alt');
+    hero.append(`<img src="${src}" alt="${alt}">`);
   });
-  
+}
+
+handleThumnailClicks();
